d4fbcfb691a6c9a0db4ac13ed409deaadfd42b06
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2023-02-18 16:24:12 +0900

    part2/12

--- Part2/Q12.php
+++ Part2/Q12.php
@@ -1,1 +1,19 @@
 <?php
+$var = "テスト";
+// function var($var) {
+//     echo $var;
+// }
+// var($var);
+function f($var) {
+    echo $var;
+}
+f($var);
+
+// 「var」は予約語なので関数名には使えない。
+// $var = "テスト";
+// function var1($var) {
+//     echo $var;
+// }
+// var1($var);
+
+// 上記でも可
