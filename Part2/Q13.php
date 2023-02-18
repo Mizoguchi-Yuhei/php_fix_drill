cad90afa8f8d1744dc7b05337fe9885307192c83
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2023-02-18 16:29:25 +0900

    part2/q14

--- Part2/Q13.php
+++ Part2/Q13.php
@@ -1,1 +1,5 @@
 <?php
+echo $_SERVER["SCRIPT_NAME"];
+
+// 「$SERVER」や「$_server」、「["script_name"]」など、
+// どこかが小文字だったり、アンダースコアがなければエラー
--- Part2/Q14.php
+++ Part2/Q14.php
@@ -1,1 +1,9 @@
 <?php
+$status = false;
+
+// if ($status == "false") :
+if ($status == false) :
+    echo "処理を停止します。";
+else :
+    echo "処理を継続します。";
+endif;
